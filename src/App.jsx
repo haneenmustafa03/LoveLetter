59b990bc13f0556d25ebbbdf66de08210d62e071
am-ndaa <148898058+am-ndaa@users.noreply.github.com>
2025-02-07 21:51:10 -0500

    changed background color and tried adding particle js
    ended up just coloring the root's background the same as the screen div's color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,67 @@
+import React from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import "./App.css";
 
 function App() {
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="screen">
       {/* Section 1 */}
-      <div className="flex items-center justify-center h-screen bg-pink-300 text-white text-7xl font-bold">
-        1
+      <div className="WelcomeSection">
+      <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: "#f9dce1",
+              },
+            },
+            particles: {
+              color: {
+                value: "#f9dec1",
+              },
+              links: {
+                color: "#f9dec1",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: true,
+                speed: 1,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+                value: 80,
+              },
+              size: {
+                random: true,
+                value: 3,
+              },
+            },
+          }}
+        />
+        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
+          <h1 className="text-5xl font-bold text-pink-500 drop-shadow-lg">
+            💖 Welcome to Our Self-Love Journey! 💖
+          </h1>
+        </div>
       </div>
 
       {/* Section 2 */}
