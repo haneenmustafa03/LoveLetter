38604b16b80626a36e8a50e97ea31102e5603c03
am-ndaa <148898058+am-ndaa@users.noreply.github.com>
2025-02-07 23:22:26 -0500

    i shouldn't have any changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,40 @@
 import React from "react";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+// import Particles from "react-tsparticles";
+// import { loadFull } from "tsparticles";
 import "./App.css";
+import { SectionTwo } from "./components/Homescreen/SectionTwo.jsx";
+import { SectionThree } from "./components/Homescreen/SectionThree";
+
 
 function App() {
-  const particlesInit = async (main) => {
-    await loadFull(main);
-  };
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-  return (
-    <div className="screen">
-      {/* Section 1 */}
-      <div className="WelcomeSection">
-      <Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={{
-            background: {
-              color: {
-                value: "#f9dce1",
-              },
-            },
-            particles: {
-              color: {
-                value: "#f9dec1",
-              },
-              links: {
-                color: "#f9dec1",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-              },
-              move: {
-                direction: "none",
-                enable: true,
-                outMode: "bounce",
-                random: true,
-                speed: 1,
-                straight: false,
-              },
-              number: {
-                density: {
-                  enable: true,
-                  value_area: 800,
-                },
-                value: 80,
-              },
-              size: {
-                random: true,
-                value: 3,
-              },
-            },
-          }}
-        />
-        <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full">
-          <h1 className="text-5xl font-bold text-pink-500 drop-shadow-lg">
-            💖 Welcome to Our Self-Love Journey! 💖
-          </h1>
-        </div>
-      </div>
+ // const particlesInit = async (main) => {
+ //   await loadFull(main);
+ // };
+ // const particlesLoaded = (container) => {
+ //   console.log(container);
+ // };
+ return (
+   <div className="screen">
+     {/* Section 1 */}
+     {/* <SectionOne /> */}
+     {/* Blah blah blah */}
 
-      {/* Section 2 */}
-      <div className="flex items-center justify-center h-screen bg-purple-300 text-white text-7xl font-bold">
-        2
-      </div>
 
-      {/* Section 3 */}
-      <div className="flex items-center justify-center h-screen bg-blue-300 text-white text-7xl font-bold">
-        3
-      </div>
-    </div>
-  );
+     {/* Section 2 */}
+     <SectionTwo />
+
+
+     {/* Section 3 */}
+     <SectionThree />
+   </div>
+ );
 }
 
+
 export default App;
+
+
+
+
+
 
